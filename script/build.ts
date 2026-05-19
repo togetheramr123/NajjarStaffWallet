3e8f3f1053e08a4121ed22456f1ae2eb24fd76f9
zezoo rasher <zezoorasher@zezoos-MacBook-Air.local>
2026-05-19 17:42:46 +0300

    fix: disable server minification to fix drizzle-orm parsing error

--- script/build.ts
+++ script/build.ts
@@ -56,7 +56,7 @@ async function buildAll() {
     define: {
       "process.env.NODE_ENV": '"production"',
     },
-    minify: true,
+    minify: false,
     external: externals,
     logLevel: "info",
   });
